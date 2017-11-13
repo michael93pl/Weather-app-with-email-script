5c5e6cf3a01c766a76fb10fb1a4edd45f7a8ec4a
michael93pl <michael93pl@gmail.com>
2017-11-13 18:46:33 +0100

    rename of email script file, finished input function

---
+++ email_script.py
@@ -0,0 +1,55 @@
+import smtplib
+import additional
+import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+
+def sending_email():
+
+    #server commends
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+
+    #credentials of sender
+    FROM = "michael93pl"
+    PASSWORD = additional.x
+
+
+    #logging in
+    server.login(FROM, PASSWORD)
+
+    #template for recievers
+    TOADDR = ["michael93pl@gmail.com"]
+    CC = []
+    SUBJECT = "testing"
+    TEXT = "TEST FOR GITHUB"
+
+    #MSG template
+
+
+    message = MIMEMultipart()
+    message['From'] = "Michal <{}>".format(FROM)
+    message['To'] = ", ".join(TOADDR)
+    message['Cc'] = ", ".join(CC)
+    message['Subject'] = SUBJECT
+    message.attach(MIMEText(TEXT))
+
+    MSG = message.as_string()
+
+    #Join reciever with CC
+
+    FINAL_TO = CC + TOADDR
+
+
+
+    server.sendmail(FROM, FINAL_TO, MSG)
+
+    TIME = datetime.datetime.now()
+    print("Email sent at {}".format(TIME))
+
+sending_email()
